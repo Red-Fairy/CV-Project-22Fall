bd170d8497879af4d08c06cbe953324e92c95d99
RedFairy <rundongluo2002@gmail.com>
2023-01-18 21:39:54 +0800

    update

--- 2-identification/train_identification.py
+++ 2-identification/train_identification.py
@@ -108,15 +108,13 @@ def validate(model, loaders):
 def main():
 	transforms_train_contrastive = transforms.Compose([
 								transforms.RandomResizedCrop(224, (0.8, 1.0)),
-								# transforms.Resize((256,256)),
-								# transforms.RandomCrop((224,224)),
 								transforms.RandomRotation(15),
 								transforms.RandomHorizontalFlip(p=0.5),
 								transforms.ColorJitter(0.2, 0.2, 0.2, 0.05),
 								transforms.ToTensor(),
 								transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))])
-	transforms_train = transforms.Compose([transforms.Resize((256,256)),
-								transforms.RandomCrop((224,224)),
+	transforms_train = transforms.Compose([transforms.RandomHorizontalFlip(p=0.5),
+								transforms.Resize((224,224)),
 								transforms.ToTensor(),
 								transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))])
 	transforms_test = transforms.Compose([transforms.ToTensor(),
